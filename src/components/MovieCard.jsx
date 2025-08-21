416796c2c3feca4a5ce9468a1177a97cd8fe61f3
Favour Egwele <favoregwele@gmail.com>
2025-08-20 14:22:35 -1200

    Rendering movie cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,25 @@
-function MovieCard({movie}) {
+function MovieCard({movie: { title, vote_average, poster_path, release_date, original_language }}) {
     return ( 
-        <p className="text-white" key={movie.id}>{movie.title}</p>
+        <div className="movie-card">
+            <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/not-available.png'} alt="title"/>
+
+            <div className="mt-4">
+                <h3>{title}</h3>
+
+                <div className="content">
+                    <div className="rating">
+                        <i class="bi bi-star-fill text-yellow-600"></i>
+                        <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+                    </div>
+
+                    <span>•</span>
+                    <p className="lang">{original_language}</p>
+
+                    <span>•</span>
+                    <p className="year">{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
