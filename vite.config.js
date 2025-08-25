424205f6fd3a2a0ecccba7766a2de53961ce9beb
Favour Egwele <favoregwele@gmail.com>
2025-08-25 11:40:06 -1200

    update deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/See_Movies/',
   plugins: [react(), tailwindcss()],
-  base: "/See_Movies/",
 })
